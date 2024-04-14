12e7d92e3ea8967e74c39155915526f48501c465
Ishan Verma <ishanv002@gmail.com>
2024-04-14 12:04:17 +0530

    Version12

--- Practice_of_Array.cpp
+++ Practice_of_Array.cpp
@@ -35,4 +35,5 @@ int main() {
         }
         cout << endl;
     }
+    cout << "Version12";
 }
--- reverse_a_string.cpp
+++ reverse_a_string.cpp
@@ -15,4 +15,5 @@ int main() {
     } else {
         cout << "NO";
     }
+    cout << "Version12";
 }
